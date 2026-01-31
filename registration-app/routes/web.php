fae541ee0067bf7a85d501921b1840c0e7f429a3
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-01-31 23:10:49 +0530

    feat: design authentication system with users, profiles, and roles

--- registration-app/routes/web.php
+++ registration-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('register', RegisterController::class)
+    ->only(['create', 'store']);
